c8f42c7640b244bd6ded6ff23c493c467acd559f
jgiron23 <jonathan.giron@hipotecario.com.sv>
2026-08-04 23:48:10 -0600

    Fix: Agregar url = env(DATABASE_URL) a schema.prisma 3

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 import "dotenv/config";
 
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
