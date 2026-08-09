4417eed488c3011266441e75a10e1756aec5bbc4
jgiron23 <jonathan.giron@hipotecario.com.sv>
2026-08-09 00:44:49 -0600

    Ajustes para agregar imagen 2

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ const port = process.env.PORT || 4001;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Routes
 app.use('/api', routes);
